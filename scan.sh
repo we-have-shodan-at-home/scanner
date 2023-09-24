588d0cc671483772278929bcd6868152b99bc353
ChillerDragon <ChillerDragon@gmail.com>
2023-09-24 12:31:13 +0200

    Output result to minecraft-servers repo

--- scan.sh
+++ scan.sh
@@ -4,7 +4,27 @@ source src/include.sh
 
 if [ -f masscan.json ]
 then
-	ccheck scan -p masscan.json servers.json
+	if [ -d minecraft-servers/tmp ]
+	then
+		rm -rf minecraft-servers/tmp
+	fi
+	mkdir -p minecraft-servers/tmp
+	ts="$(date '+%F_%H-%M')"
+	mkdir -p minecraft-servers/scans
+	if [ -s masscan.json ]
+	then
+		mv masscan.json "minecraft-servers/scans/masscan_$ts.json"
+	fi
+	i=0
+	for scan in ./minecraft-servers/scans/*.json
+	do
+		ccheck scan -p "$scan" "minecraft-servers/tmp/servers_$i.json"
+		i="$((i+1))"
+	done
+	if ! find ./minecraft-servers/tmp/ -mindepth 1 -maxdepth 1 -name '*.json' -exec jq '.[]' {} \; | jq --slurp . > minecraft-servers/servers.json
+	then
+		err "json merge failed"
+	fi
 fi
 
 if [ -f paused.conf ]
--- src/dependencies.sh
+++ src/dependencies.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
+apt_install() {
+	local _sudo=''
+	if [ ! -x "$(command -v sudo)" ]
+	then
+		_sudo='sudo '
+	fi
+	$_sudo apt-get install -y "$1"
+}
+
 dep_cargo() {
 	[ ! -x "$(command -v cargo)" ] || return
 
 	if ! curl https://sh.rustup.rs -sSf | sh -s -- -y
 	then
 		err "Failed to install cargo"
+		exit 1
+	fi
+}
+
+dep_jq() {
+	[[ ! -x "$(command -v jq)" ]] || return
+
+
+	if [ ! -x "$(command -v apt-get)" ]
+	then
+		apt_install jq
+	else
+		err "Error: please install jq"
 		exit 1
 	fi
 }
@@ -40,6 +62,7 @@ dep_ccheck() {
 check_deps() {
 	dep_ccheck
 	dep_masscan
+	dep_jq
 }
 
 check_deps
